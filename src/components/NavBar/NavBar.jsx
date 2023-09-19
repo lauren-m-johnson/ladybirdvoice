2caffd3c1cb2d8d911d8d77aeea2a95b3f16feda
lauren-m-johnson <laurenmj90@gmail.com>
2023-09-19 16:24:14 -0700

    create route for about page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export default function NavBar() {
     return (
+        <>
         <h2>LadyBird Voice Studios</h2>
+        <Link to='home'>Home</Link>
+        <Link to='about'>About</Link>
+        </>
     )
 }
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,16 @@
-import NavBar from '../../components/NavBar/NavBar'
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import About from '../About/About';
+import NavBar from '../../components/NavBar/NavBar';
+import './App.css';
 
 export default function App() {
   return (
     <>
       <NavBar />
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       <h1>Welcome</h1>
     </>
   )
