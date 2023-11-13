da747e7a5cd3219afd025227fa97fa1403f5fb7f
lauren-m-johnson <laurenmj90@gmail.com>
2023-11-13 13:23:44 -0800

    add logo and edit nav bar layout

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import './NavBar.css';
 export default function NavBar() {
     return (
         <div id='nav-bar'>
-            <h2>LadyBird Voice Studios</h2>
+            <img src='images/ladybird_logo.png' alt='LadyBird Voice Studios Logo and Title'></img>
             <div id='nav-links'>
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
