5322c1904dad84421b88f8bebf934afdfa0b6103
Lauren Johnson <laurenmj90@gmail.com>
2024-05-06 16:31:34 -0700

    add content to about section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,6 +4,29 @@ export default function About() {
     return (
         <div className='main-container'>
             <h1>About</h1>
+            <div id='about'>
+                <img src='/images/me with mic.jpg' alt='Photo of Maria Recording'></img>
+                <div id="about-bio">
+                    <p>
+                    Maria L. Johnson is an exceptional American Female Voice Actor with a talent for delivering 
+                    a soothing and captivating voice that leaves listeners spellbound. She is skilled at bringing 
+                    scripts to life with enchanting results. Maria works from her professional home studio,
+                    where she devotes her time to the world of Voiceover. With a focus on Audio Book Narration, 
+                    she also lends her voice to a plethora of other projects including Commercials, E-Learning,
+                     Documentaries, and Animated Character and Cartoon Voices. Her versatility and range make 
+                     her a highly sought-after talent in the industry.  
+                    </p>
+                    <p>
+                    Maria L. Johnson recently split her time between Northern California and New York City, where 
+                    she had an illustrious career, as an International Flight Attendant. Her love for travel has
+                     often taken her to different parts of the world. However, she confesses that her favorite 
+                     times are spent at home on the West Coast with her family. She has a passion for cooking and 
+                     enjoys sharing great meals with her loved ones. When she is not busy with her Voiceover work, 
+                     Maria also enjoys knitting and watching murder mysteries with her husband and their miniature 
+                     poodle, Lucy.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
