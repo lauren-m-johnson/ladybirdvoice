5c38ed90150a685f53488bd07e63906a58910ef2
Lauren Johnson <laurenmj90@gmail.com>
2024-05-13 11:11:24 -0700

    update home page

--- src/pages/Demos/Demos.jsx
+++ src/pages/Demos/Demos.jsx
@@ -2,8 +2,9 @@ import './Demos.css';
 
 export default function Demos() {
     return (
-        <div className='main-container'>
+        <div className='main-container'id='demos'>
         <h1>Demos</h1>
+        <img src='public\images\mic.jpg' alt='Image of Recording Mic'></img>
     </div>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,22 @@
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
-        <div className='main-container' >
-            <h1>Welcome</h1>
+        <div className='main-container' id='home_page'>
+            <h1>Welcome to the studio</h1>
+            <img src='public\images\studio_view.jpg' id='studio_img'></img>
+            <div id='home_guide'>
+                <Link to="/about" className="home_button">
+                    <button>View Bio</button>
+                </Link>
+                <Link to="/demos" className="home_button">
+                    <button>View Demos</button>
+                </Link>
+                <Link to="/contact" className="home_button">
+                    <button>Contact Me</button>
+                </Link>
+            </div>
         </div>
     )
 }
