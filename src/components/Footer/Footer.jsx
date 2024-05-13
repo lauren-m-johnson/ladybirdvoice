640e30c994d7ef9c1cdc39d371978e0797dc81e0
Lauren Johnson <laurenmj90@gmail.com>
2024-05-13 10:42:16 -0700

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,16 @@
 import './Footer.css';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
-        <div id='footer'>
-            <h4>Footer</h4>
-        </div>
-    )
+      <div id='footer'>
+        <a href="https://www.instagram.com/ladybirdvoicestudios/?utm_source=ig_embed&amp;utm_campaign=loading" target="_blank" rel='noreferrer'>
+          <img src='public\images\instagram.png' alt="Instagram" />
+        </a>
+        <p>ladybirdvoicestudios@gmail.com</p>
+        <Link to="/contact">
+        <button>Free Quote</button>
+        </Link>
+      </div>
+    );
 }
